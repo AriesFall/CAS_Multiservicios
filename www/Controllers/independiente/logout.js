41c557af6d924f825d89c7ea676747ea735cd819
AriesFall <humberto.araguz@uttn.mx>
2024-09-24 15:57:06 -0500

    Primer commit

--- www/Controllers/independiente/logout.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function logout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('correo');
-    window.location.href = '/index.html';
-  }
-  window.logout = logout;
-});
