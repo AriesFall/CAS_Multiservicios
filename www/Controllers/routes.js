25a4daa5cc76e3584f6973afa7e9aacea70cd508
AriesFall <humberto.araguz@uttn.mx>
2024-10-05 17:41:38 -0500

    Segundo commit

--- www/Controllers/routes.js
+++ www/Controllers/routes.js
@@ -3,12 +3,13 @@ const local_server = "https://localhost:44303/api/"                         //Wr
 const public_server = "http://apiescrile.somee.com/api/"                        //Write WEB API public address
 const local_sources = ""       //Write App local resources
 
-const env = public_server                                  //Select your environment (local or public server)
+const env = local_server                                  //Select your environment (local or public server)
 
 //Users API
 const allUsers_route = env + "user/users"
 const loginUser_route = env + "user/login"
-const registerUser_route = env + "user/register"
+const signupUser_route = env + "user/register"
+
 
 //Ejemplo de rutas API
 /*
